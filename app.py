fea455bf902897bf7faf4d613169e0ff40ed7be5
Jhon Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2024-05-16 23:45:49 -0500

    fixed layout errors

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import customtkinter
 
 class Setup:
 	def __init__(self,setup_name,setup_apps):
-		self.setup_name = setup_name
-		self.setup_apps = setup_apps
+		self.name = setup_name
+		self.apps = setup_apps
 class App(customtkinter.CTk):
 	def __init__(self):
 		super().__init__()
@@ -33,7 +33,31 @@ class App(customtkinter.CTk):
 		self.add_setup_button.pack()
 		self.selected_frame_by_name = 'setups'
 	def build_setups(self):
-		pass
+		dummy_setups = [{'name':'Test Setup', 'apps':['/Users/jhonidrovo/Applications/Loom.app']},{'name':'Test Setup 2', 'apps':['/Users/jhonidrovo/Applications/Loom.app']}, {'name':'Test Setup 3', 'apps':['/Users/jhonidrovo/Applications/Loom.app']}]
+		self.setups_grid = customtkinter.CTkFrame(self.main_frame)
+		self.setups_grid.grid_columnconfigure(0, pad=10, weight=1)
+		self.setups_grid.grid_columnconfigure(1, pad=10, weight=1)
+		self.setups_grid.grid_rowconfigure(0, weight=1, pad=10)
+		rows = [[]]
+		for setup in dummy_setups:
+			# setup_frame = customtkinter.CTkFrame(setups_grid)
+			setup_button = customtkinter.CTkButton(self.setups_grid, text=setup['name'])
+			row_index = len(rows)-1
+			row = rows[row_index]
+			if len(row)==2:
+				row[0].grid(column=0, row=row_index)
+				row[1].grid(column=1, row=row_index)
+				self.setups_grid.grid_rowconfigure(row_index+1, weight=1)
+				rows.append([setup_button])
+			else:
+				row.append(setup_button)
+		# Attach the last row items
+		last_row = rows[len(rows)-1]
+		for x in range(len(last_row)):
+			last_row[x].grid(column=x, row=len(rows)-1)
+
+		self.setups_grid.pack()
+		print(rows)
 
 if __name__ == "__main__":
     app = App()
